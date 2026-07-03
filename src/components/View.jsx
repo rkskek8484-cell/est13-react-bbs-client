3f0604735d297ad6a9252c18b6abc5fc8152b920
rkskek8484-cell <rkskek8484@gmail.com>
2026-07-03 11:24:56 +0900

    feat: Implement View component to display post details with data fetching

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -5,22 +5,44 @@ import { Link, useParams } from 'react-router';
 
 export default function View() {
   const [content, setContent] = useState({
-    name: '',
+    writer: '',
     title: '',
     content: '',
+    date: '',
   });
   const { id } = useParams();
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/view?id=${id}`)
+      .then((response) => {
+        console.log(response.data);
+        // setContent(response.data)
+        const data = response.data[0];
+
+        setContent({
+          writer: data.writer,
+          title: data.title,
+          content: data.content,
+          date: data.date,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        console.log('요청완료');
+      });
+  }, []);
 
   return (
     <>
-      <h2>글제목</h2>
+      <h2>{content.title}</h2>
       <div className='d-flex justify-content-between'>
-        <p>글쓴이</p>
-        <p>날짜</p>
+        <p>글쓴이: {content.writer}</p>
+        <p>{content.date}</p>
       </div>
       <hr />
-      본문
+      {content.content}
       <hr />
       <div className='d-flex gap-1 justify-content-end'>
         <Link to='/write' className='btn btn-primary'>
